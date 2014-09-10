78567b0d84667e8d085a02f9c998cf5b321b2bdc
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2014-09-10 15:54:31 -0400

    home page now working right

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
 	has_many :memberships
 	has_many :clubs, through: :memberships
+	validates :l_name, :uniqueness => { :scope => :f_name, :case_sensitive => false }
+	# validates :f_name, uniqueness: true
+	# validates :l_name, uniqueness: true
+	validates :dob, uniqueness: true
+	# validates :club_id, uniqueness: true
 end
