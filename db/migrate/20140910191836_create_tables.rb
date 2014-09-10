b0fddfac4557d7c14b49d50803e03732741b0466
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2014-09-10 15:24:45 -0400

    added tables

---
+++ db/migrate/20140910191836_create_tables.rb
@@ -0,0 +1,19 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :clubs do |t|
+    	t.string   :name
+    	t.text     :description
+    	t.boolean  :accepting
+    end
+    create_table :memberships do |t|
+    	t.integer  :club_id
+    	t.integer  :user_id
+    end
+    create_table :users do |t|
+    	t.string   :f_name
+    	t.string   :l_name
+    	t.date     :dob
+    end
+  end
+
+end
