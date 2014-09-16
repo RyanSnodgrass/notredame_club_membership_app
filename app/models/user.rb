a39e48f751df7bccbbfc9d27e4fbd1e4d1ba7ccd
RyanSnodgrass <ryansnodgrass.dev@gmail.com>
2014-09-15 20:36:30 -0400

    converted all tabs to proper spaces

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	has_many :memberships
-	has_many :clubs, through: :memberships
-	validates :l_name, :uniqueness => { :scope => :f_name, :case_sensitive => false }
+  has_many :memberships
+  has_many :clubs, through: :memberships
+  validates :l_name, :uniqueness => { :scope => :f_name, :case_sensitive => false }
 
-	validates :dob, uniqueness: true
+  validates :dob, uniqueness: true
 end
--- db/migrate/20140910191836_create_tables.rb
+++ db/migrate/20140910191836_create_tables.rb
@@ -1,18 +1,18 @@
 class CreateTables < ActiveRecord::Migration
   def change
     create_table :clubs do |t|
-    	t.string   :name
-    	t.text     :description
-    	t.boolean  :accepting
+        t.string   :name
+        t.text     :description
+        t.boolean  :accepting
     end
     create_table :memberships do |t|
-    	t.integer  :club_id
-    	t.integer  :user_id
+        t.integer  :club_id
+        t.integer  :user_id
     end
     create_table :users do |t|
-    	t.string   :f_name
-    	t.string   :l_name
-    	t.date     :dob
+        t.string   :f_name
+        t.string   :l_name
+        t.date     :dob
     end
   end
 
